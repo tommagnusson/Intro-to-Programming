7550d7a635216e51e5292772c0ee86356e739431
Tommy Magnusson <tommymagnusson@148-100-148-186.FoxNet.marist.edu>
2016-11-10 15:51:12 -0500

    Adding actual files

---
+++ work_duty copy.js
@@ -0,0 +1,24 @@
+const dutiesBookStore = ["Register", "Textbooks", "Bathroom Cleaning", "Helping Customers", "Alphabetizing Books"];
+
+function onload() {
+  var stepper = document.getElementById("stepper");
+  stepper.setAttribute("max", dutiesBookStore.length);
+  stepper.setAttribute("min", 0);
+}
+
+function renderDutiesBookStore() {
+  
+  var numDuties = document.getElementById("stepper").value;
+  
+  var arrNumDuties = [];
+  for(var i = 0; i<numDuties; i++) {
+    arrNumDuties.push(i);
+  }
+  
+  var duties = arrNumDuties.map((a) => dutiesBookStore[a]);
+  var para = document.getElementById("dutiesHere");
+  para.innerHTML = duties.join(", ");
+  
+  var button = document.getElementById("buttonBookStore");
+  button.setAttribute("disabled", "disabled");
+}
