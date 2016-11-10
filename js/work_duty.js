4d70989a51472967b63a749a82a97c83ae6fa552
Marist User <Marist User>
2016-11-10 18:50:07 -0500

    trying to figure out stepper attr, commented out

--- js/work_duty.js
+++ js/work_duty.js
@@ -1,23 +1,29 @@
 const dutiesBookStore = ["Register", "Textbooks", "Bathroom Cleaning", "Helping Customers", "Alphabetizing Books"];
 
 function onload() {
-  var stepper = document.getElementById("stepper");
-  stepper.setAttribute("max", dutiesBookStore.length);
-  stepper.setAttribute("min", 0);
+  // TODO:
+  // var stepper = document.getElementById("stepper");
+  //
+  // // Dynamically make sure max is at most the amount of jobs in the array
+  // stepper.setAttribute("max", String(dutiesBookStore.length));
+  // stepper.setAttribute("min", String(0));
 }
 
 function renderDutiesBookStore() {
+  var para = document.getElementById("dutiesHere");
+  para.innerHTML = ""; // clear the previous duties (if any)
+
   var numDuties = document.getElementById("stepper").value;
 
   var arrNumDuties = [];
+
+  // make the array 0..<stepper value
   for(var i = 0; i < numDuties; i++) {
     arrNumDuties.push(i);
   }
 
+  // map the corresponding indexes to the duties from the duties array
   var duties = arrNumDuties.map((a) => dutiesBookStore[a]);
-  var para = document.getElementById("dutiesHere");
-  para.innerHTML = duties.join(", ");
 
-  var button = document.getElementById("buttonBookStore");
-  button.setAttribute("disabled", "disabled");
+  para.innerHTML = duties.join(", "); // formatting, nicely joined by ", "
 }
