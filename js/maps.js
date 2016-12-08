843a373f7e080a99596b70e1ff04e4619e41d8c2
Marist User <Marist User>
2016-12-08 18:54:13 -0500

    got map on screen

--- js/maps.js
+++ js/maps.js
@@ -1,8 +1,8 @@
 function initialize() {
   var options = {
-    center:
-    zoom:
-    mapTypeId:
-  }
-  var map = google.maps.Map(document.getElementById("map"));
+    center: {lat: 41.7227 , lng: -73.93463},
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map"), options);
 }
