e6fefb92d8fcb639814f8a976cba97e3ebd25629
Marist User <Marist User>
2016-12-08 19:22:56 -0500

    little text box thingy

--- js/maps.js
+++ js/maps.js
@@ -1,8 +1,20 @@
+const HERE = {lat: 41.7227 , lng: -73.93463};
+
 function initialize() {
   var options = {
-    center: {lat: 41.7227 , lng: -73.93463},
+    center: HERE,
     zoom: 18,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map = new google.maps.Map(document.getElementById("map"), options);
+  var marker = new google.maps.Marker({
+      position: HERE,
+      animation: google.maps.Animation.DROP,
+      map: map
+  });
+
+  var window = new google.maps.InfoWindow({
+      content: "Our Fearless Leader" // <img src='https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/3/000/004/363/3ff8ce2.jpg'></img>"
+  });
+  marker.addListener('click', function() { window.open(map, marker)});
 }
